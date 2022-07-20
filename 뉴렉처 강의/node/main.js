119d9a3f1853d7951387e1777722e6a736f19177
Koo SangMo <rn0614@naver.com>
2022-07-20 23:35:45 +0900

    sss

--- 뉴렉처 강의/node/main.js
+++ 뉴렉처 강의/node/main.js
@@ -5,44 +5,70 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url; // queryString이 들어가는 부분
     var queryData = url.parse(_url, true).query;
+    var pathname =url.parse(_url, true).pathname;
     var title = queryData.id;
     var content ='';
-    console.log(queryData);
-    if(_url == '/'){
-      title="welcome";
-    }
-    if(_url == '/favicon.ico'){
+
+    console.log(url.parse(_url, true).pathname);
+    if(pathname==='/'){
+        if(queryData.id===undefined){
+            title='Welcome';
+            content='Hello javaScript';
+            response.writeHead(200);
+            var template=`
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="?id=HTML">HTML</a></li>
+                <li><a href="?id=CSS">CSS</a></li>
+                <li><a href="?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p style="margin-top:45px;">${content}
+            </p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        }else{
+            fs.readFile(`data/${title}`, 'utf8', function(err, data){
+                content=data;
+                console.log(data);
+                response.writeHead(200);
+                var template=`
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="?id=HTML">HTML</a></li>
+                    <li><a href="?id=CSS">CSS</a></li>
+                    <li><a href="?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p style="margin-top:45px;">${content}
+                </p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    }else{
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not Found');
     }
-    fs.readFile(`data/${title}`, 'utf8', function(err, data){
-        content=data;
-        console.log(data);
-        response.writeHead(200);
-        var template=`
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="?id=HTML">HTML</a></li>
-            <li><a href="?id=CSS">CSS</a></li>
-            <li><a href="?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">
-        <img src="coding.jpg" width="100%">
-        </p><p style="margin-top:45px;">${content}
-        </p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
 });
 app.listen(3000);
