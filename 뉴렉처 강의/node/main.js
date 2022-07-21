6c2911ff5a229a80c7956ebfed1f3b9310209678
Koo SangMo <rn0614@naver.com>
2022-07-21 22:56:22 +0900

    ss

--- 뉴렉처 강의/node/main.js
+++ 뉴렉처 강의/node/main.js
@@ -2,85 +2,54 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist){
+    var list='<ul>';
+    var i=0;
+    while(i<filelist.length){
+        list = list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i+=1
+    }
+    list=list+'</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url; // queryString이 들어가는 부분
     var queryData = url.parse(_url, true).query;
     var pathname =url.parse(_url, true).pathname;
-    var title = queryData.id;
-    var content ='';
-
-    console.log(url.parse(_url, true).pathname);
     if(pathname==='/'){
         if(queryData.id===undefined){
-            console.log('체크포인트1');
             fs.readdir('./data', function(err, filelist){
-
-                title='Welcome';
-                content='Hello javaScript';
-                var list='<ul>';
-                var i=0;
-                while(i<filelist.length){
-                    list = list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                    i+=1
-                }
-                list=list+'</ul>';
-
-                response.writeHead(200);
-                var template=`
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p style="margin-top:45px;">${content}
-                </p>
-                </body>
-                </html>
-                `;
+                var title='Welcome';
+                var content='Hello javaScript';
+                var list = templateList(filelist);
+                var template= templateHTML(title, list, `<h2>${title}</h2>${content}`);
                 response.writeHead(200);
                 response.end(template);
             })
         }else{
             fs.readdir('./data', function(err, filelist){
-                var list='<ul>';
-                var i=0;
-                while(i<filelist.length){
-                    list = list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                    i+=1
-                }
-                list=list+'</ul>';
-                fs.readFile(`data/${title}`, 'utf8', function(err, data){
-                    content=data;
-                    console.log(data);
-                    response.writeHead(200);
-                    var list='<ul>';
-                    var i=0;
-                    while(i<filelist.length){
-                        list = list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                        i+=1
-                    }
-                    list=list+'</ul>';
-                    var template=`
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p style="margin-top:45px;">${content}
-                    </p>
-                    </body>
-                    </html>
-                    `;
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, content){
+                    var title= queryData.id;
+                    var list = templateList(filelist);
+                    var template= templateHTML(title, list, `<h2>${title}</h2>${content}`);
                     response.writeHead(200);
                     response.end(template);
                 })
